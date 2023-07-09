cabe856cb6c95246b0c0ec0eb83f33182de0e2b9
Gabriela Machado <gbms4078@gmail.com>
2023-07-09 18:24:52 -0500

    Creando cambios

--- src/app/modules/listas/lista-basica/lista-basica.component.ts
+++ src/app/modules/listas/lista-basica/lista-basica.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { timeout } from 'rxjs';
+
+export type Proucto = {
+
+  nombre: string;
+  precio: number;
+  descripcion: string;
+}
 
 @Component({
   selector: 'app-lista-basica',
   templateUrl: './lista-basica.component.html',
   styleUrls: ['./lista-basica.component.css']
 })
-export class ListaBasicaComponent {
+export class ListaBasicaComponent implements  OnInit {
+  listaElemento: Proucto[]= [{
+
+    nombre: 'leche',
+    precio: 2,
+    descripcion: 'Leche procesada en mercurio '
+  },
+{
+  nombre:'Carne',
+  precio: 10,
+descripcion:'Carne de lomo de la madre de duque jeje  '
+},
+{
+
+  nombre:'Pollo',
+  precio: 20,
+descripcion:'Lumier '
+}
+];
+  
+  cargando: boolean = true ; 
+textAlign: { [klass: string]: any; }|null|undefined;
+  cambiarCargando(){
+    this.cargando =!this.cargando
+  }
+
+  //ngSwitch
+  opcion: number = 0;
+
+  //funcion
+  escogerOpcion(opcionEscogida: number){
+    this.opcion =opcionEscogida; //dado que valor cambia iplica un cambio en los elementos 
+  }
+
+
+  ngOnInit(): void {
+ //   timeout 
+  }
+ 
+
 
 }
